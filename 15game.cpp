ecc44f3f352f373964c79794c712512e9cf59200
Ras-Kur <92600019+Ras-Kur@users.noreply.github.com>
2021-11-19 02:32:48 +0300

    становится похоже на игру.

--- 15game.cpp
+++ 15game.cpp
@@ -3,16 +3,14 @@
 #include <vector>
 #include<conio.h>
 
-void Menu()
+int Menu_pole()
 {
-	// выбор режима 3х3 4х4 5х5 9х9
+	
+}
 
-	// собирает человек или робот или вместе
-
-	// перемешивает человек или рандом
 
 	//сколько времени птрачено. и сколько шагов
-}
+
 
 int Swipe(int arr1[], int arr2[], int s)
 {
@@ -79,36 +77,41 @@ void print_arr_rand(int buff_arr[], int enter_size, int size)
 
 int main()
 {
+
+	/*time_t seconds;
+	seconds = time(NULL);
+	time_t second2;
+	second2 = time(NULL);
+	int res_time = (second2 - seconds) / 60;
+	*/
 	setlocale(LC_ALL, "ru");								 //			русская локализация, может пригодится
 	
 	system("cls");											 //			чистим консоль перед началом									
-			
-	Logo();													 //		главное лого Пятнашки
+	
+	srand(time(NULL));					  					 //			ядро времени для постоянного рандома
 
+	Logo();													 //			главное лого Пятнашки
+	
 	std::cout << "\n\n\t\t\t нажмите клавишу \"Enter\"...";
 	std::cin.get();
 	
-	system("cls");											 //		отчищаем от лого и пресаникея
+	system("cls");											 //			отчищаем от лого и пресаникея
 	
 	
 
-	srand(time(NULL));					  					 //		ядро времени для постоянного рандома
+	
 
 	bool cheсk_double;										 // 		для уникального рандома без повторов
 
-	int enter_size = 3;										 //		выбор пользователем размера поля
+	int enter_size = 3;										 //			выбор пользователем размера поля
 
 	int size = enter_size * enter_size;						 // 		для использования во всей программе
 
-	std::vector < std::vector<int>> pole 					 // 		новый двумерный вектор для поля
-
-	(enter_size, std::vector<int>(enter_size));			     //			перенос строки(указываю размер)
-
-	int* buff_arr = new int[size]; 							 //		использую для для временного хранения
+	int* buff_arr = new int[size]; 							 //			использую для для временного хранения
 
 	int* arr_while = new int[size];							 //			массив для выхода из цикла while
 
-	for (int i = 0; i <= size; i++)							 //		заполняю массив от единицы по порядку
+	for (int i = 0; i <= size; i++)							 //			заполняю массив от единицы по порядку
 		arr_while[i] = i + 1;
 
 	arr_while[size - 1] = 0;								 //			последнее значение меняю на ноль
@@ -117,6 +120,16 @@ int main()
 
 	for (size_t i = 0; i < size; i++)						 //			заполняю массив уникальными знач-ми (-1)
 		buff_arr[i] = -1;
+
+	int x = -enter_size, y = -enter_size;									//	перевод начала отсчёта вниз в право к нулю
+
+	int& a1 = buff_arr[0], & a2 = buff_arr[1], & a3 = buff_arr[2];
+	int& a4 = buff_arr[3], & a5 = buff_arr[4], & a6 = buff_arr[5];			//	делаю ссылки на все значения массива
+	int& a7 = buff_arr[6], & a8 = buff_arr[7], & a9 = buff_arr[8];
+
+	int& a10 = buff_arr[9], & a11 = buff_arr[10], & a12 = buff_arr[11];		//	жостко, но жизнь - сложная штука. 
+	int& a13 = buff_arr[12], & a14 = buff_arr[13], & a15 = buff_arr[14];
+	int& a16 = buff_arr[15];
 
 	//  заполняю динамический массив рандомом
 	//             \|/
@@ -155,7 +168,7 @@ int main()
 
 	//это красивый вывод, ну как красивый, приятно глазу.
 	//               \|/
-	for (size_t i = 0; i < size; i++)
+	/*for (size_t i = 0; i < size; i++)
 	{
 		if (enter_size > 3)
 		{
@@ -171,38 +184,233 @@ int main()
 		{
 			std::cout << "\n";
 		}
-	}
+	}*/
 
-	int x = -enter_size, y = -enter_size;						        //	 перевод начала отсчёта вниз в право к нулю
-
-	int& a1 = buff_arr[0], & a2 = buff_arr[1], & a3 = buff_arr[2];
-	int& a4 = buff_arr[3], & a5 = buff_arr[4], & a6 = buff_arr[5];		  // делаю ссылки на все значения массива
-	int& a7 = buff_arr[6], & a8 = buff_arr[7], & a9 = buff_arr[8];      
 	
-	int& a10= buff_arr[9], & a11= buff_arr[10], & a12= buff_arr[11];	//	 жостко, но жизнь - сложная штука. 
-	int& a13= buff_arr[12], & a14= buff_arr[13], & a15= buff_arr[14];
-	int& a16 = buff_arr[15];
 
-	Swipe(buff_arr, arr_while, size);								      // вывод первого варианта рандома 	
+	//Swipe(buff_arr, arr_while, size);								      // вывод первого варианта рандома 	
+
+
+
 	
-	int choice = _getch();	
-	while (Swipe(buff_arr, arr_while, size)==0)
+	bool flag = true;
+	int a = -1;						//	выбор размера поля
+	do
 	{
-		switch (_getch())
+		std::cout << "\n\n\t\t ВЫ В ГЛАВНОМ МЕНЮ\n";
+		std::cout << "\n\n\t  выберите размер игрового поля (сложность)...\n\n";
+		std::cout << "\n..  1  -  поле 3х3 (лёгкий режим)...";
+		std::cout << "\n..  2  -  поле 4х4 (обычный режим)...";
+		std::cout << "\n..  3  -  поле 5х5 (сложный режим)- сначала пройдите 4х4";
+		std::cout << "\n..  4  -  поле 9х9 (демонический режим)- в разработке...";
+		std::cout << "\n..  0  -  поле 0х0 (не играть и просто выйти из игры)...\n\n\t\t";
+		std::cin >> a;
+		if (a >= 0 && a < 3)
 		{
-		case 75:// left
-			system("cls");
-			Step(a9, a8);
-			print_arr_rand(buff_arr, enter_size, size);
-			break;
-		default:
-			break;
+			flag = false;
+		}
+		else
+		{
+			flag = true;
+		}
+	} while (flag);			
+	return a;
+
+
+
+
+	bool flag = true;
+	int b = -1;						// собирает человек или робот
+	do
+	{
+
+		std::cout << "\n\n\t\t выберите, кто будет разгадывать головоломку..";
+		std::cout << "\n..  1  - Вы, своими руками и мастерством...";
+		std::cout << "\n..  2  - Я, машина которая знает все ходы...\n\n\t\t";
+		std::cin >> b;
+		if (b > 0 && b < 3)
+		{
+			flag = false;
+		}
+		else
+		{
+			flag = true;
+		}
+	} while (flag);
+	return b;
+
+
+
+
+	bool flag = true;
+	int c = -1;						// перемешивает человек или рандом
+	do
+	{
+
+		std::cout << "\n\n\t\t выберите, кто будет перемешивать цыфры..";
+		std::cout << "\n..  1  - Вы, своими руками (долго и скучно)...";
+		std::cout << "\n..  2  - Я, моментально и бесподобно...\n\n\t\t";
+		std::cin >> c;
+		if (c > 0 && c < 3)
+		{
+			flag = false;
+		}
+		else
+		{
+			flag = true;
+		}
+	} while (flag);
+	return c;
+
+
+
+
+	int choice = _getch();	
+	if (a == 1 && b == 1);
+	{																//3x3
+		while (Swipe(buff_arr, arr_while, size) == 0)
+		{
+			switch (_getch())
+			{
+			case 75://лево
+				x++;
+				break;
+			case 77://право
+				x--;
+				break;
+			case 72://верх
+				y++;
+				break;//вниз
+			case 80:
+				y--;
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
+
+			switch (x)
+			{
+			case 75://лево
+
+				break;
+			case 77://право
+
+				break;
+			case 72://верх
+
+				break;//вниз
+			case 80:
+
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
+
+			switch (y)
+			{
+			case 75://лево
+
+				break;
+			case 77://право
+
+				break;
+			case 72://верх
+
+				break;//вниз
+			case 80:
+
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
+		}
+	}if (a == 2 && b == 1)
+	{																//4x4
+		while (Swipe(buff_arr, arr_while, size) == 0)
+		{
+			switch (_getch())
+			{
+			case 75://лево
+				x++;
+				break;
+			case 77://право
+				x--;
+				break;
+			case 72://верх
+				y++;
+				break;//вниз
+			case 80:
+				y--;
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
+
+			switch (x)
+			{
+			case 75://лево
+
+				break;
+			case 77://право
+
+				break;
+			case 72://верх
+
+				break;//вниз
+			case 80:
+
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
+
+			switch (y)
+			{
+			case 75://лево
+
+				break;
+			case 77://право
+
+				break;
+			case 72://верх
+
+				break;//вниз
+			case 80:
+
+				break;
+			case 27://ескейп
+				return 0;
+			default:
+				std::cout << "\n\n     управление - на стрелки, \"esc\" - выход.";
+				break;
+			}
 		}
 	}
 	
 	
+	
 	/*do
 	
+	        system("cls");
+			Step(a9, a8);
+			print_arr_rand(buff_arr, enter_size, size);
+
+
 			case 72: //вверх
 			case 80: //вниз
 			case 75: //влево
