064f6876c64e5c698f5a2c316b83556788137fe7
Ras-Kur <92600019+Ras-Kur@users.noreply.github.com>
2021-11-18 19:33:52 +0300

    изменил но не робит всёровно

--- 15game.cpp
+++ 15game.cpp
@@ -1,87 +1,84 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<ctime>
 #include <vector>
 #include<conio.h>
 
+bool Swipe(int arr1[], int arr2[], int s)
+{
+	int flag = 0;
+	for (int i = 0; i < s; i++)
+	{
+		if (arr1[i] != arr2[i])
+		{
+			flag + 1;
+		}
+		flag = -1;
+		if (flag==s)
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
 int main()
 {
-	setlocale(LC_ALL, "ru");						 //		русская локализация, может пригодится
 
-	srand(time(NULL));				    			   //	ядро времени для постоянного рандома
+	
+	setlocale(LC_ALL, "ru");						 //			СЂСѓСЃСЃРєР°СЏ Р»РѕРєР°Р»РёР·Р°С†РёСЏ, РјРѕР¶РµС‚ РїСЂРёРіРѕРґРёС‚СЃСЏ
 
-	bool cheсk_double;								 // 	для уникального рандома без повторов
+	srand(time(NULL));				    			   //		СЏРґСЂРѕ РІСЂРµРјРµРЅРё РґР»СЏ РїРѕСЃС‚РѕСЏРЅРЅРѕРіРѕ СЂР°РЅРґРѕРјР°
 
-	int enter_size = 3;								   //	выбор пользователем размера поля
+	bool cheСЃk_double;								 // 		РґР»СЏ СѓРЅРёРєР°Р»СЊРЅРѕРіРѕ СЂР°РЅРґРѕРјР° Р±РµР· РїРѕРІС‚РѕСЂРѕРІ
 
-	int size = enter_size * enter_size;				 // 	для использования во всей программе
+	int enter_size = 3;								   //		РІС‹Р±РѕСЂ РїРѕР»СЊР·РѕРІР°С‚РµР»РµРј СЂР°Р·РјРµСЂР° РїРѕР»СЏ
 
-	std::vector < std::vector<int>> pole 			   // 	новый двумерный вектор для поля
+	int size = enter_size * enter_size;				 // 		РґР»СЏ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ РІРѕ РІСЃРµР№ РїСЂРѕРіСЂР°РјРјРµ
 
-	(enter_size, std::vector<int>(enter_size));      //		перенос строки(указываю размер)
+	std::vector < std::vector<int>> pole 			   // 		РЅРѕРІС‹Р№ РґРІСѓРјРµСЂРЅС‹Р№ РІРµРєС‚РѕСЂ РґР»СЏ РїРѕР»СЏ
 
-	int* buff_arr = new int[size]; 					   //	использую для для временного хранения
+	(enter_size, std::vector<int>(enter_size));      //			РїРµСЂРµРЅРѕСЃ СЃС‚СЂРѕРєРё(СѓРєР°Р·С‹РІР°СЋ СЂР°Р·РјРµСЂ)
 
-	int* arr_while = new int[size];					 //		массив для выхода из цикла while
+	int* buff_arr = new int[size]; 					   //		РёСЃРїРѕР»СЊР·СѓСЋ РґР»СЏ РґР»СЏ РІСЂРµРјРµРЅРЅРѕРіРѕ С…СЂР°РЅРµРЅРёСЏ
 
-	for (int i = 0; i <= size; i++)				       //	заполняю массив от единицы по порядку
+	int* arr_while = new int[size];					 //			РјР°СЃСЃРёРІ РґР»СЏ РІС‹С…РѕРґР° РёР· С†РёРєР»Р° while
+
+	for (int i = 0; i <= size; i++)				       //		Р·Р°РїРѕР»РЅСЏСЋ РјР°СЃСЃРёРІ РѕС‚ РµРґРёРЅРёС†С‹ РїРѕ РїРѕСЂСЏРґРєСѓ
 		arr_while[i] = i + 1;
 
-	arr_while[size - 1] = 0;							 //		последнее значение меняю на ноль
+	arr_while[size - 1] = 0;							 //		РїРѕСЃР»РµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ РјРµРЅСЏСЋ РЅР° РЅРѕР»СЊ
 
-	int buff_num = 0;						    	   //	переменная для буфера значения
+	int buff_num = 0;						    	   //		РїРµСЂРµРјРµРЅРЅР°СЏ РґР»СЏ Р±СѓС„РµСЂР° Р·РЅР°С‡РµРЅРёСЏ
 
-	for (size_t i = 0; i < size; i++)				 //		заполняю массив уникальными знач-ми (-1)
+	for (size_t i = 0; i < size; i++)				     //		Р·Р°РїРѕР»РЅСЏСЋ РјР°СЃСЃРёРІ СѓРЅРёРєР°Р»СЊРЅС‹РјРё Р·РЅР°С‡-РјРё (-1)
 		buff_arr[i] = -1;
 
+	std::cout<<Swipe(buff_arr, arr_while, size);
 
-
-	//  заполняю динамический массив рандомом
+	//  Р·Р°РїРѕР»РЅСЏСЋ РґРёРЅР°РјРёС‡РµСЃРєРёР№ РјР°СЃСЃРёРІ СЂР°РЅРґРѕРјРѕРј
 	//             \|/
 	for (int i = 0; i < size; )
 	{
-		cheсk_double = true;
+		cheСЃk_double = true;
 		int	rand_num = rand() % size;
 
 		for (int j = 0; j < i; j++)
 		{
 			if (buff_arr[j] == rand_num)
 			{
-				cheсk_double = false;
+				cheСЃk_double = false;
 				break;
 			}
 		}
-		if (cheсk_double)
+		if (cheСЃk_double)
 		{
 			buff_arr[i] = rand_num;
 			i++;
 		}
 	}
 
-
-	/*do
-	{
-		//заполнить
-		switch (_getch())                // 13 enter    32 backspase    27 escape
-		{
-			case 72:	(0, -1);         break; //вверх
-			case 80:	(0, 1);          break; //вниз
-			case 75:	(-1, 0);		 break; //влево
-			case 77:	(1, 0);			 break; //вправо
-			case 32:                     break; //пробел
-		}
-	} while (true);*/
-	int arr[5];
-	for (size_t i = 0; i < 5; i++)
-	{
-		arr[i] = _getch();
-		std::cout << arr[i] << " ";
-	}
-
-
-
-
-	//   ставлю ноль в конец с заменой чисел
-	//              \|/
+	//   СЃС‚Р°РІР»СЋ РЅРѕР»СЊ РІ РєРѕРЅРµС† СЃ Р·Р°РјРµРЅРѕР№ С‡РёСЃРµР»
+//              \|/
 	for (size_t i = 0; i < size; i++)
 	{
 		if (buff_arr[i] == 0 && buff_arr[i] != buff_arr[size - 1])
@@ -94,25 +91,50 @@ int main()
 	}
 
 
-	//это красивый вывод, ну как красивый, приятно глазу.
+	//СЌС‚Рѕ РєСЂР°СЃРёРІС‹Р№ РІС‹РІРѕРґ, РЅСѓ РєР°Рє РєСЂР°СЃРёРІС‹Р№, РїСЂРёСЏС‚РЅРѕ РіР»Р°Р·Сѓ.
 	//               \|/
 	for (size_t i = 0; i < size; i++)
 	{
 		if (enter_size > 3)
 		{
 			if (buff_arr[i] <= 9)
-				std::cout << "0" << buff_arr[i] << " ";     // это не просто костыль, - это ХОДУЛИ!!!))))
+				std::cout << "0" << buff_arr[i] << " ";     // СЌС‚Рѕ РЅРµ РїСЂРѕСЃС‚Рѕ РєРѕСЃС‚С‹Р»СЊ, - СЌС‚Рѕ РҐРћР”РЈР›Р!!!))))
 			else
 				std::cout << buff_arr[i] << " ";
 		}
 		else
 			std::cout << buff_arr[i] << " ";
 
-		if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // скорее всего тоже можно иначе.
+		if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // СЃРєРѕСЂРµРµ РІСЃРµРіРѕ С‚РѕР¶Рµ РјРѕР¶РЅРѕ РёРЅР°С‡Рµ.
 		{
 			std::cout << "\n";
 		}
 	}
+
+
+	/*do
+	{
+		//Р·Р°РїРѕР»РЅРёС‚СЊ
+		switch (_getch())                // 13 enter    32 backspase    27 escape
+		{
+			case 72:	(0, -1);         break; //РІРІРµСЂС…
+			case 80:	(0, 1);          break; //РІРЅРёР·
+			case 75:	(-1, 0);		 break; //РІР»РµРІРѕ
+			case 77:	(1, 0);			 break; //РІРїСЂР°РІРѕ
+			case 32:                     break; //РїСЂРѕР±РµР»
+		}
+	} while (true);*/
+	int arr[5];
+	for (size_t i = 0; i < 5; i++)
+	{
+		arr[i] = _getch();
+		std::cout << arr[i] << " ";
+	}
+
+
+
+
+
 
 	int x = -3;
 	int y = -2;
@@ -124,9 +146,7 @@ int main()
 	
 	
 
-	 
-
-	while (arr_while==buff_arr);
+	do
 	{
 		system("cls");
 		switch (choice)
@@ -137,206 +157,214 @@ int main()
 		default:
 			break;
 		}
-		std::cout << x;
-		if (y == -2)
+
+
+
+	} while (Swipe(buff_arr, arr_while, size)==1);
 		{
-			switch (x)
+
+
+			std::cout << x;
+			if (y == -2)
 			{
-			case -2:
-				if (x == -1)
+				switch (x)
 				{
-					buff_step = buff_arr[7];
-					buff_arr[7] = buff_arr[8];
-					buff_arr[8] = buff_step;
-					x--;
-					for (size_t i = 0; i < size; i++)
+				case -2:
+					if (x == -1)
 					{
-						if (enter_size > 3)
+						buff_step = buff_arr[7];
+						buff_arr[7] = buff_arr[8];
+						buff_arr[8] = buff_step;
+						x--;
+						for (size_t i = 0; i < size; i++)
 						{
-							if (buff_arr[i] <= 9)
-								std::cout << "0" << buff_arr[i] << " ";     // это не просто костыль, - это ХОДУЛИ!!!))))
+							if (enter_size > 3)
+							{
+								if (buff_arr[i] <= 9)
+									std::cout << "0" << buff_arr[i] << " ";     // СЌС‚Рѕ РЅРµ РїСЂРѕСЃС‚Рѕ РєРѕСЃС‚С‹Р»СЊ, - СЌС‚Рѕ РҐРћР”РЈР›Р!!!))))
+								else
+									std::cout << buff_arr[i] << " ";
+							}
 							else
 								std::cout << buff_arr[i] << " ";
-						}
-						else
-							std::cout << buff_arr[i] << " ";
 
-						if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // скорее всего тоже можно иначе.
-						{
-							std::cout << "\n";
+							if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // СЃРєРѕСЂРµРµ РІСЃРµРіРѕ С‚РѕР¶Рµ РјРѕР¶РЅРѕ РёРЅР°С‡Рµ.
+							{
+								std::cout << "\n";
+							}
 						}
 					}
-				}
-				else
-				{
-					buff_step = buff_arr[8];
-					buff_arr[8] = buff_arr[7];
-					buff_arr[7] = buff_step;
-					x++;
-					for (size_t i = 0; i < size; i++)
+					else
 					{
-						if (enter_size > 3)
+						buff_step = buff_arr[8];
+						buff_arr[8] = buff_arr[7];
+						buff_arr[7] = buff_step;
+						x++;
+						for (size_t i = 0; i < size; i++)
 						{
-							if (buff_arr[i] <= 9)
-								std::cout << "0" << buff_arr[i] << " ";     // это не просто костыль, - это ХОДУЛИ!!!))))
+							if (enter_size > 3)
+							{
+								if (buff_arr[i] <= 9)
+									std::cout << "0" << buff_arr[i] << " ";     // СЌС‚Рѕ РЅРµ РїСЂРѕСЃС‚Рѕ РєРѕСЃС‚С‹Р»СЊ, - СЌС‚Рѕ РҐРћР”РЈР›Р!!!))))
+								else
+									std::cout << buff_arr[i] << " ";
+							}
 							else
 								std::cout << buff_arr[i] << " ";
-						}
-						else
-							std::cout << buff_arr[i] << " ";
 
-						if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // скорее всего тоже можно иначе.
-						{
-							std::cout << "\n";
+							if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // СЃРєРѕСЂРµРµ РІСЃРµРіРѕ С‚РѕР¶Рµ РјРѕР¶РЅРѕ РёРЅР°С‡Рµ.
+							{
+								std::cout << "\n";
+							}
 						}
 					}
-				}
 
-				break; //правый край (3х3)
+					break; //РїСЂР°РІС‹Р№ РєСЂР°Р№ (3С…3)
 
-			case -1:
-				if (x == -2)
-				{
-					buff_step = buff_arr[8];
-					buff_arr[8] = buff_arr[7];
-					buff_arr[7] = buff_step;
-					x++;
-					for (size_t i = 0; i < size; i++)
+				case -1:
+					if (x == -2)
 					{
-						if (enter_size > 3)
+						buff_step = buff_arr[8];
+						buff_arr[8] = buff_arr[7];
+						buff_arr[7] = buff_step;
+						x++;
+						for (size_t i = 0; i < size; i++)
 						{
-							if (buff_arr[i] <= 9)
-								std::cout << "0" << buff_arr[i] << " ";     // это не просто костыль, - это ХОДУЛИ!!!))))
+							if (enter_size > 3)
+							{
+								if (buff_arr[i] <= 9)
+									std::cout << "0" << buff_arr[i] << " ";     // СЌС‚Рѕ РЅРµ РїСЂРѕСЃС‚Рѕ РєРѕСЃС‚С‹Р»СЊ, - СЌС‚Рѕ РҐРћР”РЈР›Р!!!))))
+								else
+									std::cout << buff_arr[i] << " ";
+							}
 							else
 								std::cout << buff_arr[i] << " ";
-						}
-						else
-							std::cout << buff_arr[i] << " ";
 
-						if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // скорее всего тоже можно иначе.
-						{
-							std::cout << "\n";
+							if (i == enter_size - 1 || i == enter_size * 2 - 1 || i == enter_size * 3 - 1)  // СЃРєРѕСЂРµРµ РІСЃРµРіРѕ С‚РѕР¶Рµ РјРѕР¶РЅРѕ РёРЅР°С‡Рµ.
+							{
+								std::cout << "\n";
+							}
 						}
 					}
-				}
-				else
-				{
-					buff_step = buff_arr[6];
-					buff_arr[6] = buff_arr[7];
-					buff_arr[7] = buff_step;
-					x--;
-				}
+					else
+					{
+						buff_step = buff_arr[6];
+						buff_arr[6] = buff_arr[7];
+						buff_arr[7] = buff_step;
+						x--;
+					}
 
-				break; //середина
+					break; //СЃРµСЂРµРґРёРЅР°
 
-			case 0:
-				if (x == -1)
-				{
-					buff_step = buff_arr[7];
-					buff_arr[7] = buff_arr[6];
-					buff_arr[6] = buff_step;
-					x++;
+				case 0:
+					if (x == -1)
+					{
+						buff_step = buff_arr[7];
+						buff_arr[7] = buff_arr[6];
+						buff_arr[6] = buff_step;
+						x++;
+					}
+					break; //Р»РµРІС‹Р№ РєСЂР°Р№
+
+				default:
+					std::cout << " \nРґРµР№СЃС‚РІРёРµ РЅРµ РІРѕР·РјРѕР¶РЅРѕ.";
+					break;
 				}
-				break; //левый край
-
-			default:
-				std::cout << " \nдействие не возможно.";
-				break;
 			}
-		}
-		if (y == -1)
-		{
-			switch (x)
+			if (y == -1)
 			{
-			case -2:
-				if (x == -1)
+				switch (x)
 				{
-					buff_step = buff_arr[4];
-					buff_arr[4] = buff_arr[5];
-					buff_arr[5] = buff_step;
-					x--;
-				}
-				break; //правый край (3х3)
+				case -2:
+					if (x == -1)
+					{
+						buff_step = buff_arr[4];
+						buff_arr[4] = buff_arr[5];
+						buff_arr[5] = buff_step;
+						x--;
+					}
+					break; //РїСЂР°РІС‹Р№ РєСЂР°Р№ (3С…3)
 
-			case -1:
-				if (x == -2)
-				{
-					buff_step = buff_arr[5];
-					buff_arr[5] = buff_arr[4];
-					buff_arr[4] = buff_step;
-					x++;
-				}
-				else
-				{
-					buff_step = buff_arr[3];
-					buff_arr[3] = buff_arr[4];
-					buff_arr[4] = buff_step;
-					x--;
-				}
+				case -1:
+					if (x == -2)
+					{
+						buff_step = buff_arr[5];
+						buff_arr[5] = buff_arr[4];
+						buff_arr[4] = buff_step;
+						x++;
+					}
+					else
+					{
+						buff_step = buff_arr[3];
+						buff_arr[3] = buff_arr[4];
+						buff_arr[4] = buff_step;
+						x--;
+					}
 
-				break; //середина
+					break; //СЃРµСЂРµРґРёРЅР°
 
-			case 0:
-				if (x == -1)
-				{
-					buff_step = buff_arr[4];
-					buff_arr[4] = buff_arr[3];
-					buff_arr[3] = buff_step;
-					x++;
+				case 0:
+					if (x == -1)
+					{
+						buff_step = buff_arr[4];
+						buff_arr[4] = buff_arr[3];
+						buff_arr[3] = buff_step;
+						x++;
+					}
+					break; //Р»РµРІС‹Р№ РєСЂР°Р№
+
+				default:
+					std::cout << " \nРґРµР№СЃС‚РІРёРµ РЅРµ РІРѕР·РјРѕР¶РЅРѕ.";
+					break;
 				}
-				break; //левый край
-
-			default:
-				std::cout << " \nдействие не возможно.";
-				break;
 			}
-		}
-		if (y == 0)
-		{
-			switch (x)
+			if (y == 0)
 			{
-			case -2:
-				if (x == -1)
+				switch (x)
 				{
-					buff_step = buff_arr[1];
-					buff_arr[1] = buff_arr[2];
-					buff_arr[2] = buff_step;
-					x--;
-				}
-				break; //правый край (3х3)
+				case -2:
+					if (x == -1)
+					{
+						buff_step = buff_arr[1];
+						buff_arr[1] = buff_arr[2];
+						buff_arr[2] = buff_step;
+						x--;
+					}
+					break; //РїСЂР°РІС‹Р№ РєСЂР°Р№ (3С…3)
 
-			case -1:
-				if (x == -2)
-				{
-					buff_step = buff_arr[2];
-					buff_arr[2] = buff_arr[1];
-					buff_arr[1] = buff_step;
-					x++;
-				}
-				else
-				{
-					buff_step = buff_arr[0];
-					buff_arr[0] = buff_arr[1];
-					buff_arr[1] = buff_step;
-					x--;
-				}
+				case -1:
+					if (x == -2)
+					{
+						buff_step = buff_arr[2];
+						buff_arr[2] = buff_arr[1];
+						buff_arr[1] = buff_step;
+						x++;
+					}
+					else
+					{
+						buff_step = buff_arr[0];
+						buff_arr[0] = buff_arr[1];
+						buff_arr[1] = buff_step;
+						x--;
+					}
 
-				break; //середина
+					break; //СЃРµСЂРµРґРёРЅР°
 
-			case 0:
-				if (x == -1)
-				{
-					buff_step = buff_arr[1];
-					buff_arr[1] = buff_arr[0];
-					buff_arr[0] = buff_step;
-					x++;
+				case 0:
+					if (x == -1)
+					{
+						buff_step = buff_arr[1];
+						buff_arr[1] = buff_arr[0];
+						buff_arr[0] = buff_step;
+						x++;
+					}
+					break; //Р»РµРІС‹Р№ РєСЂР°Р№
+
+				default:
+					std::cout << " \nРґРµР№СЃС‚РІРёРµ РЅРµ РІРѕР·РјРѕР¶РЅРѕ.";
+					break;
 				}
-				break; //левый край
-
-			default:
-				std::cout << " \nдействие не возможно.";
-				break;
 			}
 		}
-	}
+	
 }
